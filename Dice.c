d59d3275a3ef3e448c6c29b50265bdbf709ea320
Daiki Kanno <s1260049@std2dc5.u-aizu.ac.jp>
2019-07-05 12:24:26 +0900

    Won/lost message added

--- Dice.c
+++ Dice.c
@@ -12,6 +12,7 @@ int main(){
   printf("Die 1: %d\n",dice1);
   printf("Die 2: %d\n",dice2);
   printf("Total value: %d\n",dice1+dice2);
-
+ if((dice1+dice2)>7)printf("You won!\n");
+  else printf("You lost!\n");
   return 0;
 }
